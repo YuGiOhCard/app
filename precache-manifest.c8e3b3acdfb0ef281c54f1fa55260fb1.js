c125edd9a465c1414da18f4f889b95e079a9e06b
YuGiOhCardDev <96167449+YuGiOhCardDev@users.noreply.github.com>
2021-12-31 00:25:26 +0800

    init YGOCard App

---
+++ precache-manifest.c8e3b3acdfb0ef281c54f1fa55260fb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd94100031d9bb2c63010242d701b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25663f41faeda290b4e4",
+    "url": "/static/css/main.b476affd.chunk.css"
+  },
+  {
+    "revision": "050931229ba86aa115d2",
+    "url": "/static/js/2.ed765e38.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/static/js/2.ed765e38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25663f41faeda290b4e4",
+    "url": "/static/js/main.72127cda.chunk.js"
+  },
+  {
+    "revision": "6ccfa69cb323cfada2f4",
+    "url": "/static/js/runtime-main.822ce4ae.js"
+  }
+]);
